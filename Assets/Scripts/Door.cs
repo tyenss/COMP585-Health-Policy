e24f0cd9d99b4a042734086e43afc7211928f26b
tyenss <yuuki.butler@gmail.com>
2020-02-25 03:00:03 -0500

    added queueing and fixed some problems that occured with z axis

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -1,11 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class Door : MonoBehaviour
 {
     public bool active;
-    public Queue<Patient> playerQueue;
+    public Vector2 coordsToPlace;
+    public Vector2 officeCoords;
+    private Queue<Patient> playerQueue;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,8 +24,39 @@ public class Door : MonoBehaviour
 
     void OnMouseDown()
     {
-        //Add something that detects the client
-        Patient player = GameObject.FindObjectOfType(typeof(Patient)) as Patient;
-        //playerQueue.
+        Patient player = FindObjectOfType(typeof(Patient)) as Patient;
+        if (!playerQueue.Contains(player))
+        {
+            AddToQueue(player);
+        }
+    }
+
+    public void AddToQueue(Patient patient)
+    {
+        patient.transform.position = new Vector3(
+            coordsToPlace.x,
+            coordsToPlace.y - (float)(playerQueue.Count() * 100),
+            patient.transform.position.z);
+        playerQueue.Enqueue(patient);
+    }
+
+    /// <summary>
+    /// Pops patient
+    /// </summary>
+    /// <returns>patient in front of queue. If queue is empty, returns null</returns>
+    public Patient PopQueue()
+    {
+        if (playerQueue.Peek() == null)
+        {
+            return null;
+        }
+        Patient popped = playerQueue.Dequeue();
+        foreach (Patient patient in playerQueue)
+        {
+            patient.transform.position = new Vector3(coordsToPlace.x,
+                coordsToPlace.y + 100,
+                patient.transform.position.z);
+        }
+        return popped;
     }
 }
--- Assets/Scripts/SceneMove.cs
+++ Assets/Scripts/SceneMove.cs
@@ -4,14 +4,34 @@ using UnityEngine;
 
 public class SceneMove : MonoBehaviour
 {
+    public Vector3 waitingRoom;
+    public Door door;
+
     public void MoveToOffice(Door door, Vector3 newPosition)
     {
-        Patient patient = door.playerQueue.Dequeue();
+        Patient patient = door.PopQueue();
         patient.gameObject.transform.position = newPosition;
     }
 
     public void MoveToLobby(Patient patient, Vector3 newPosition)
     {
         patient.gameObject.transform.position = newPosition;
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha9))
+        {
+            //move to office
+            Patient patient = door.PopQueue();
+            patient.transform.position = door.officeCoords;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            //move to lobby
+            Patient player = FindObjectOfType(typeof(Patient)) as Patient; //Fix for networking code
+            player.transform.position = waitingRoom;
+        }
     }
 }
