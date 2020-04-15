9fcabb02c25528c39837add38e33bc69d2e9911c
jake <jlbutler1997@gmail.com>
2020-04-15 03:01:51 -0400

    cameras for tablet

---
+++ Assets/Scripts/TitleScreen.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using UnityEngine.Networking;
+
+public class TitleScreen : NetworkBehaviour
+{
+    public Camera Cam1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cam1.enabled = true;
+        Cam1.aspect = (Screen.currentResolution.width / Screen.currentResolution.height);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
