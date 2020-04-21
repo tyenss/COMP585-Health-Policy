01032a52ad1a1bfad1d74e32c0981cb1b9c5cebe
madeleinenieto <46536250+madeleinenieto@users.noreply.github.com>
2020-04-20 21:17:48 -0400

    updates

---
+++ Assets/Scripts/PriceChanges.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PriceChanges : MonoBehaviour
+{
+    public Text changingPrice;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void changePrice() {
+        changingPrice.text = ""
+    }
+}
